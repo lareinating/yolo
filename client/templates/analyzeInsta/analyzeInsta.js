5001387eb82d19d3069a509bbe843d618a70aece
lareinating <lareinating@gmail.com>
2015-11-07 21:34:36 -0800

    add more skeleton files

--- client/templates/analyzeInsta/analyzeInsta.js
+++ client/templates/analyzeInsta/analyzeInsta.js
@@ -1,83 +1,83 @@
-var api_key = '969713533.b6f7db4.1af42be3f40e422fb5b33105eb21b967';
-var imageArray;
-var tagArray;
+// var api_key = '969713533.b6f7db4.1af42be3f40e422fb5b33105eb21b967';
+// var imageArray;
+// var tagArray;
 
-var getLatestIGPhoto = function(api_key){
-    var url = "https://api.instagram.com/v1/users/self/media/recent/?access_token=" + api_key;
-    var tagArray = [];
-    $.ajax({
-        dataType: "jsonp",
-        url: url,
-        success: function(data) {
-            var imageKeyWords = groupMultipleAlchemyTags(data.data);
-        }
-    });
-}
+// var getLatestIGPhoto = function(api_key){
+//     var url = "https://api.instagram.com/v1/users/self/media/recent/?access_token=" + api_key;
+//     var tagArray = [];
+//     $.ajax({
+//         dataType: "jsonp",
+//         url: url,
+//         success: function(data) {
+//             var imageKeyWords = groupMultipleAlchemyTags(data.data);
+//         }
+//     });
+// }
 
-var getSingleAlchemyTags = function(imageURL){
-    var alchemyEndPoint = "https://gateway-a.watsonplatform.net/calls/url/URLGetRankedImageKeywords";
-    $.ajax({
-       url: alchemyEndPoint,
-       dataType: 'json',
-       type: 'GET',
-       data: {
-           format: 'json',
-           url: imageURL,
-           apikey: '75b4cc895c87c2854fb066f0e99514f762d5c819',
-           outputMode: 'json',
-       },
-       success: function(data) {
-           return data.imageKeywords.text;
-       }
-    });
-}
+// var getSingleAlchemyTags = function(imageURL){
+//     var alchemyEndPoint = "https://gateway-a.watsonplatform.net/calls/url/URLGetRankedImageKeywords";
+//     $.ajax({
+//        url: alchemyEndPoint,
+//        dataType: 'json',
+//        type: 'GET',
+//        data: {
+//            format: 'json',
+//            url: imageURL,
+//            apikey: '75b4cc895c87c2854fb066f0e99514f762d5c819',
+//            outputMode: 'json',
+//        },
+//        success: function(data) {
+//            return data.imageKeywords.text;
+//        }
+//     });
+// }
 
-var groupMultipleAlchemyTags = function (igData) {
-    var alchemyEndPoint = "https://gateway-a.watsonplatform.net/calls/url/URLGetRankedImageKeywords";
-    var tagArray = [];
-    var internal_index = 0;
-    var photo_count = 0;
+// var groupMultipleAlchemyTags = function (igData) {
+//     var alchemyEndPoint = "https://gateway-a.watsonplatform.net/calls/url/URLGetRankedImageKeywords";
+//     var tagArray = [];
+//     var internal_index = 0;
+//     var photo_count = 0;
     
-    for (var i=0; i< igData.length; i++){
-        media = igData[i];
-        if (media.type == 'image'){
-            photo_count += 1;
-            $.ajax({
-               url: alchemyEndPoint,
-               dataType: 'json',
-               type: 'GET',
-               data: {
-                   format: 'json',
-                   url: igData[i].images.standard_resolution.url,
-                   apikey: 'be39ddc9e6982581f885815e8e5636b38b5b9a37',
-                   outputMode: 'json',
-               },
-               success: function(data) {
-                   console.log(data);
-                   var keywords = data.imageKeywords;
-                   internal_index += 1;
-                   console.log(keywords);
-                   for ( var j=0; j<keywords.length; j++){
-                       if (tagArray.indexOf(keywords[j].text) == -1){
-                           tagArray.push(keywords[j].text);
-                       }
-                   }
-                   console.log(internal_index)
-                   if (internal_index == photo_count){
-                       getCategory(tagArray);
-                   }
-               }
-            });
-        }
-    }
-}
+//     for (var i=0; i< igData.length; i++){
+//         media = igData[i];
+//         if (media.type == 'image'){
+//             photo_count += 1;
+//             $.ajax({
+//                url: alchemyEndPoint,
+//                dataType: 'json',
+//                type: 'GET',
+//                data: {
+//                    format: 'json',
+//                    url: igData[i].images.standard_resolution.url,
+//                    apikey: 'be39ddc9e6982581f885815e8e5636b38b5b9a37',
+//                    outputMode: 'json',
+//                },
+//                success: function(data) {
+//                    // console.log(data);
+//                    var keywords = data.imageKeywords;
+//                    internal_index += 1;
+//                    console.log(keywords);
+//                    for ( var j=0; j<keywords.length; j++){
+//                        if (tagArray.indexOf(keywords[j].text) == -1){
+//                            tagArray.push(keywords[j].text);
+//                        }
+//                    }
+//                    console.log(internal_index)
+//                    if (internal_index == photo_count){
+//                        getCategory(tagArray);
+//                    }
+//                }
+//             });
+//         }
+//     }
+// }
 
-getLatestIGPhoto(api_key);
+// getLatestIGPhoto(api_key);
 
-var getCategory = function(tagArray){
+// var getCategory = function(tagArray){
 
-  // tagArray is the  array of tags returned by bluemix(alchemy)
-    console.log(tagArray);
-}
+//   // tagArray is the  array of tags returned by bluemix(alchemy)
+//     console.log(tagArray);
+// }
 
 
---
+++ client/templates/confirmation/confirmation.js
@@ -0,0 +1,19 @@
+Template.comfirmation.rendered = function() {
+// <script type="text/javascript" src="//api.skyscanner.net/api.ashx?key=0c535f89-a861-48a2-aab2-e67b893edcd0"></script>
+// <script type="text/javascript">
+//    skyscanner.load("snippets","2");
+//    function main(){
+//        var snippet = new skyscanner.snippets.SearchPanelControl();
+//        snippet.setShape("box400x400");
+//        snippet.setCulture("en-GB");
+//        snippet.setCurrency("USD");
+//        snippet.setMarket("US");
+//        snippet.setProduct("hotels","1");
+//        snippet.setProduct("carhire","2");
+//        snippet.setCarHireStartLocation("DXB", false);
+//        snippet.draw(document.getElementById("snippet_searchpanel"));
+//    }
+//    skyscanner.setOnLoadCallback(main);
+// </script>
+// <div id="snippet_searchpanel" style="width: auto; height:auto;"></div>
+}
