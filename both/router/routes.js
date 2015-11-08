681e2d25890d541e8420ea868d3253b4c8a8e1e6
lareinating <lareinating@gmail.com>
2015-11-07 19:27:04 -0800

    create skeleton pages for all page

--- both/router/routes.js
+++ both/router/routes.js
@@ -7,6 +7,31 @@ Router.route('/dashboard', {
   controller: 'DashboardController'
 });
 
+Router.route('/analyzeInsta', {
+  name: 'analyzeInsta',
+});
+
+Router.route('/yoloChoice', {
+  name: 'yoloChoice',
+});
+Router.route('/animationPage', {
+  name: 'animationPage',
+});
+
+Router.route('/comfirmation', {
+  name: 'comfirmation',
+});
+Router.route('/finalAcceptance', {
+  name: 'finalAcceptance',
+});
+
+Router.route('/finalAcceptanceCharge', {
+  name: 'finalAcceptanceCharge',
+});
+Router.route('/prizePage', {
+  name: 'prizePage',
+});
+
 Router.plugin('ensureSignedIn', {
   only: ['dashboard']
 });
